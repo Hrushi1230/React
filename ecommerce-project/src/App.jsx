5f73aaefa023191e34a24d05318f0728893547e1
Hrushikesh <hrushisahu973@gmail.com>
2025-08-26 13:58:59 +0530

    generate html from cart items

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -4,13 +4,23 @@ import {Routes , Route} from 'react-router';
 import { CheckoutPage } from './pages/CheckoutPage';
 import { Orders } from './pages/Orders';
 import { Tracking } from './pages/tracking';
+import { useEffect,useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [cart,setCart] =useState([]);
+       useEffect(()=>{
+       axios.get('/api/cart-items?expand=product')
+          .then((response)=>{
+            setCart(response.data)
+          })
+        },[]);
+
 
   return (
     <Routes>
-      <Route path='/' element={ <HomePage/>}>  </Route>
-      <Route path='/checkout' element={<CheckoutPage/>}></Route>
+      <Route path='/' element={ <HomePage cart={cart}/>}>  </Route>
+      <Route path='/checkout' element={<CheckoutPage cart={cart}/>}></Route>
       <Route path='/orders' element={<Orders/>}></Route>
       <Route path='/tracking' element={<Tracking/>}></Route>
     </Routes>
--- ecommerce-project/src/pages/CheckoutPage.jsx
+++ ecommerce-project/src/pages/CheckoutPage.jsx
@@ -1,6 +1,8 @@
 import './checkout.css';
 import './checkout-header.css';
-export function CheckoutPage(){
+
+
+export function CheckoutPage({cart}){
     return(
         <>
          <title>Checkout</title>
@@ -15,7 +17,7 @@ export function CheckoutPage(){
 
                 <div className="checkout-header-middle-section">
                 Checkout (<a className="return-to-home-link"
-                    href="/">3 items</a>)33
+                    href="/">3 items</a>)
                 </div>
 
                 <div className="checkout-header-right-section">
@@ -29,101 +31,28 @@ export function CheckoutPage(){
 
             <div className="checkout-grid">
                 <div className="order-summary">
-                <div className="cart-item-container">
-                    <div className="delivery-date">
-                    Delivery date: Tuesday, June 21
-                    </div>
 
-                    <div className="cart-item-details-grid">
-                    <img className="product-image"
-                        src="images/products/athletic-cotton-socks-6-pairs.jpg" />
-
-                    <div className="cart-item-details">
-                        <div className="product-name">
-                        Black and Gray Athletic Cotton Socks - 6 Pairs
-                        </div>
-                        <div className="product-price">
-                        $10.90
-                        </div>
-                        <div className="product-quantity">
-                        <span>
-                            Quantity: <span className="quantity-label">2</span>
-                        </span>
-                        <span className="update-quantity-link link-primary">
-                            Update
-                        </span>
-                        <span className="delete-quantity-link link-primary">
-                            Delete
-                        </span>
-                        </div>
-                    </div>
-
-                    <div className="delivery-options">
-                        <div className="delivery-options-title">
-                        Choose a delivery option:
-                        </div>
-                        <div className="delivery-option">
-                        <input type="radio" checked
-                            className="delivery-option-input"
-                            name="delivery-option-1" />
-                        <div>
-                            <div className="delivery-option-date">
-                            Tuesday, June 21
-                            </div>
-                            <div className="delivery-option-price">
-                            FREE Shipping
-                            </div>
-                        </div>
-                        </div>
-                        <div className="delivery-option">
-                        <input type="radio"
-                            className="delivery-option-input"
-                            name="delivery-option-1" />
-                        <div>
-                            <div className="delivery-option-date">
-                            Wednesday, June 15
-                            </div>
-                            <div className="delivery-option-price">
-                            $4.99 - Shipping
-                            </div>
-                        </div>
-                        </div>
-                        <div className="delivery-option">
-                        <input type="radio"
-                            className="delivery-option-input"
-                            name="delivery-option-1" />
-                        <div>
-                            <div className="delivery-option-date">
-                            Monday, June 13
-                            </div>
-                            <div className="delivery-option-price">
-                            $9.99 - Shipping
-                            </div>
-                        </div>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-
-                <div className="cart-item-container">
+                   { cart.map((cartItem)=>{
+                      return(
+                        <div  key={cartItem.productId} className="cart-item-container">
                     <div className="delivery-date">
                     Delivery date: Wednesday, June 15
                     </div>
 
                     <div className="cart-item-details-grid">
                     <img className="product-image"
-                        src="images/products/intermediate-composite-basketball.jpg" />
+                        src={cartItem.product.image} />
 
                     <div className="cart-item-details">
                         <div className="product-name">
-                        Intermediate Size Basketball
+                        {cartItem.product.name}
                         </div>
                         <div className="product-price">
-                        $20.95
+                        {(cartItem.product.priceCents/100).toFixed(2)}
                         </div>
                         <div className="product-quantity">
                         <span>
-                            Quantity: <span className="quantity-label">1</span>
+                            Quantity: <span className="quantity-label">{cartItem.quantity}</span>
                         </span>
                         <span className="update-quantity-link link-primary">
                             Update
@@ -178,6 +107,9 @@ export function CheckoutPage(){
                     </div>
                     </div>
                 </div>
+                       
+                      )
+                    })};
                 </div>
 
                 <div className="payment-summary">
