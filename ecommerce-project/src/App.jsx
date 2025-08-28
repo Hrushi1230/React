11893cedf78b4faa1251d4b105b86416ebfaa115
Hrushikesh <hrushisahu973@gmail.com>
2025-08-28 14:46:02 +0530

    generate html in orderpage

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -13,15 +13,16 @@ function App() {
        axios.get('/api/cart-items?expand=product')
           .then((response)=>{
             setCart(response.data)
+           
           })
         },[]);
 
-
+ console.log(cart);
   return (
     <Routes>
       <Route path='/' element={ <HomePage cart={cart}/>}>  </Route>
       <Route path='/checkout' element={<CheckoutPage cart={cart}/>}></Route>
-      <Route path='/orders' element={<Orders/>}></Route>
+      <Route path='/orders' element={<Orders cart={cart}/>}></Route>
       <Route path='/tracking' element={<Tracking/>}></Route>
     </Routes>
   )
